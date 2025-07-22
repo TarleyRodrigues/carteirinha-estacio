c92e8a5ac33fc303552e4b027be38b3948bc59eb
TarleyRodrigues <tarley.divino@gmail.com>
2025-07-22 11:53:23 -0300

    Refactor import order and clean up comments in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,13 @@
-# app/__init__.py
-from app.routes import bp
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy  # Adicionar import
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# Adicionar configuração do BD
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-# Necessário para sessões e formulários
 app.config['SECRET_KEY'] = 'uma-chave-secreta-muito-dificil'
 
-db = SQLAlchemy(app)  # Inicializar o BD
+db = SQLAlchemy(app)
 
+from app.routes import bp
 app.register_blueprint(bp)
+
+from app import models
